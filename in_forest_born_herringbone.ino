04351044b57e9002ce696a5216a9a64437c594a7
Egor Ivin <ivinegor88@gmail.com>
2022-06-25 01:01:32 +0300

    Add songs

---
+++ in_forest_born_herringbone.ino
@@ -0,0 +1,197 @@
+// Ноты
+#define REST 0
+#define NOTE_B0 31
+#define NOTE_C1 33
+#define NOTE_CS1 35
+#define NOTE_D1 37
+#define NOTE_DS1 39
+#define NOTE_E1 41
+#define NOTE_F1 44
+#define NOTE_FS1 46
+#define NOTE_G1 49
+#define NOTE_GS1 52
+#define NOTE_A1 55
+#define NOTE_AS1 58
+#define NOTE_B1 62
+#define NOTE_C2 65
+#define NOTE_CS2 69
+#define NOTE_D2 73
+#define NOTE_DS2 78
+#define NOTE_E2 82
+#define NOTE_F2 87
+#define NOTE_FS2 93
+#define NOTE_G2 98
+#define NOTE_GS2 104
+#define NOTE_A2 110
+#define NOTE_AS2 117
+#define NOTE_B2 123
+#define NOTE_C3 131
+#define NOTE_CS3 139
+#define NOTE_D3 147
+#define NOTE_DS3 156
+#define NOTE_E3 165
+#define NOTE_F3 175
+#define NOTE_FS3 185
+#define NOTE_G3 196
+#define NOTE_GS3 208
+#define NOTE_A3 220
+#define NOTE_AS3 233
+#define NOTE_B3 247
+#define NOTE_C4 262
+#define NOTE_CS4 277
+#define NOTE_D4 294
+#define NOTE_DS4 311
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_FS4 370
+#define NOTE_G4 392
+#define NOTE_GS4 415
+#define NOTE_A4 440
+#define NOTE_AS4 466
+#define NOTE_B4 494
+#define NOTE_C5 523
+#define NOTE_CS5 554
+#define NOTE_D5 587
+#define NOTE_DS5 622
+#define NOTE_E5 659
+#define NOTE_F5 698
+#define NOTE_FS5 740
+#define NOTE_G5 784
+#define NOTE_GS5 831
+#define NOTE_A5 880
+#define NOTE_AS5 932
+#define NOTE_B5 988
+#define NOTE_C6 1047
+#define NOTE_CS6 1109
+#define NOTE_D6 1175
+#define NOTE_DS6 1245
+#define NOTE_E6 1319
+#define NOTE_F6 1397
+#define NOTE_FS6 1480
+#define NOTE_G6 1568
+#define NOTE_GS6 1661
+#define NOTE_A6 1760
+#define NOTE_AS6 1865
+#define NOTE_B6 1976
+#define NOTE_C7 2093
+#define NOTE_CS7 2217
+#define NOTE_D7 2349
+#define NOTE_DS7 2489
+#define NOTE_E7 2637
+#define NOTE_F7 2794
+#define NOTE_FS7 2960
+#define NOTE_G7 3136
+#define NOTE_GS7 3322
+#define NOTE_A7 3520
+#define NOTE_AS7 3729
+#define NOTE_B7 3951
+#define NOTE_C8 4186
+#define NOTE_CS8 4435
+#define NOTE_D8 4699
+#define NOTE_DS8 4978
+ 
+// Пины подключенных модулей
+#define LED_R_PIN 10
+#define LED_G_PIN 9
+#define LED_B_PIN 4
+#define LED_Y_PIN 6
+#define BUZZER_PIN 7
+#define BUTTON_PIN 8
+ 
+// Массив с нотами мелодии
+int melody[] = {
+  NOTE_C4, NOTE_A4, NOTE_A4, NOTE_G4,
+  NOTE_A4, NOTE_F4, NOTE_C4, NOTE_C4,
+  NOTE_C4, NOTE_A4, NOTE_A4, NOTE_AS4,
+  NOTE_G4, NOTE_C5, 0, NOTE_C5, NOTE_D4,
+  NOTE_D4, NOTE_AS4,NOTE_AS4,NOTE_A4,
+  NOTE_G4, NOTE_F4, NOTE_C4, NOTE_A4,
+  NOTE_A4, NOTE_G4, NOTE_A4, NOTE_F4
+};
+ 
+// Массив длительности нот в мелодии: 
+// 4 = четвертная нота, 8 = восьмая нота, и так далее...
+int noteDurations[] = {
+  4,4,4,4
+  4,4,4,4,
+  4,4,4,4,
+  4,6,2,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,6
+};
+ 
+void setup() {
+    // Пины со светодиодами в режим выхода
+    pinMode(LED_R_PIN, OUTPUT);
+    pinMode(LED_Y_PIN, OUTPUT);
+    pinMode(LED_G_PIN, OUTPUT);
+    pinMode(LED_B_PIN, OUTPUT);
+}
+ 
+void loop() {
+    // Если была нажата кнопка, запускаем мелодию
+    if (checkClick())
+        startMelody();
+ 
+    // Гасим светодиоды
+    digitalWrite(LED_R_PIN, LOW);
+    digitalWrite(LED_G_PIN, LOW);
+    digitalWrite(LED_Y_PIN, LOW);
+    digitalWrite(LED_B_PIN, LOW);
+}
+ 
+void startMelody() {
+    // Вычисляем количество нот
+    int noteCount = sizeof(melody) / sizeof(int);
+    // Переменная для хранения состояния светодиодов
+    bool toggleLeds = true;
+    // Играем мелодию
+    for (int thisNote = 0; thisNote < noteCount; thisNote++) {
+        // Если была нажата клавиша - останавливаем мелодию
+        if (checkClick())
+            return;
+        // Каждую новую ноту меняем состояние светодиодов
+        digitalWrite(LED_R_PIN, toggleLeds);
+        digitalWrite(LED_Y_PIN, toggleLeds);
+        digitalWrite(LED_G_PIN, !toggleLeds);
+        digitalWrite(LED_B_PIN, !toggleLeds);
+        toggleLeds = !toggleLeds;
+        // Останавливаем воспроизведение предыдущей ноты
+        noTone(BUZZER_PIN);
+ 
+        // Вычислим длительность ноты. Разделим секунду на длительность ноты из массива
+        // Четвертная нота = 1000 / 4, восьмая нота 1000 / 8 
+        // Скорость можно увеличить, увеличив числитель. Больше подошло 1200
+        int noteDuration = 1200 / noteDurations[thisNote];
+ 
+        // Можно увеличить высоту мелодии, умножая ноты на 2, 4, 8
+        int octaveMul = 2;
+ 
+        // Воспроизведём ноту
+        tone(BUZZER_PIN, octaveMul * melody[thisNote], noteDuration);
+ 
+        // Чтобы отделить ноты друг от друга, добавим небольшую паузу между ними
+        // 30% от длины ноты звучат неплохо
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+    }
+}
+ 
+// Функция для проверки клика кнопки
+bool checkClick() {
+    // Переменная для хранения предыдущего состояния кнопки
+    static bool buttonWasUp = true;
+    bool result = false;
+    // Читаем текущее состояние кнопки
+    bool buttonIsUp = digitalRead(BUTTON_PIN);
+ 
+    // Если кнопка была не нажата, а теперь нажата
+    // значит был клик
+    if (buttonWasUp && !buttonIsUp) {
+        delay(50);
+        result = true;
+    }
+    buttonWasUp = buttonIsUp;
+    return result;
+}
